9a2d86d60d1b2b33a187f06fc147c610a24b876c
filippovk997 <filippovk997@mail.ru>
2023-01-17 13:18:41 +0400

    updated methods adverts wb

--- src/WildberriesAdvert.php
+++ src/WildberriesAdvert.php
@@ -18,8 +18,41 @@ class WildberriesAdvert extends WildberriesAdvertClient
      *
      * @return array
      */
-    public function getCount(): array
+    public function getCount(): mixed
     {
         return (new WildberriesData($this->getResponse('adv/v0/count')))->data;
+    }
+
+    /**
+     * Список РК
+     *
+     * @return array
+     */
+    public function getAdverts(
+        int $status = null,
+        int $type = null,
+        int $limit = null,
+        int $offset = null,
+        string $order = null,
+        string $direction = null,
+    ): mixed
+    {
+        return (new WildberriesData($this->getResponse(
+            'adv/v0/adverts',
+            array_diff(compact('status', 'type', 'limit', 'offset', 'order', 'direction'), [''])
+        )))->data;
+    }
+
+    /**
+     * Информация о РК
+     *
+     * @return array
+     */
+    public function getAdvert(int $id): mixed
+    {
+        return (new WildberriesData($this->getResponse(
+            'adv/v0/advert',
+            compact('id')
+        )))->data;
     }
 }
