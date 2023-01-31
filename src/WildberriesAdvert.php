9dfc97387011df6649d9a72198627ae8581c3287
filippovk997 <filippovk997@mail.ru>
2023-01-31 18:53:03 +0400

    added method getCpm

--- src/WildberriesAdvert.php
+++ src/WildberriesAdvert.php
@@ -15,6 +15,7 @@ class WildberriesAdvert extends WildberriesAdvertClient
 
     /**
      * Получение РК
+     * Получение количества рекламных кампаний (РК) поставщика
      *
      * @return array
      */
@@ -25,7 +26,14 @@ class WildberriesAdvert extends WildberriesAdvertClient
 
     /**
      * Список РК
+     * Получение списка РК поставщика
      *
+     * @param int status 
+     * @param int type
+     * @param int limit
+     * @param int offset
+     * @param string order
+     * @param string direction
      * @return array
      */
     public function getAdverts(
@@ -35,8 +43,7 @@ class WildberriesAdvert extends WildberriesAdvertClient
         int $offset = null,
         string $order = null,
         string $direction = null,
-    ): mixed
-    {
+    ): mixed {
         return (new WildberriesData($this->getResponse(
             'adv/v0/adverts',
             array_diff(compact('status', 'type', 'limit', 'offset', 'order', 'direction'), [''])
@@ -45,7 +52,9 @@ class WildberriesAdvert extends WildberriesAdvertClient
 
     /**
      * Информация о РК
-     *
+     * Получение информации об одной РК
+     * 
+     * @param int id
      * @return array
      */
     public function getAdvert(int $id): mixed
@@ -53,6 +62,22 @@ class WildberriesAdvert extends WildberriesAdvertClient
         return (new WildberriesData($this->getResponse(
             'adv/v0/advert',
             compact('id')
+        )))->data;
+    }
+
+    /**
+     * Список ставок
+     * Получение списка РК поставщика
+     *
+     * @param int type
+     * @param int param
+     * @return array
+     */
+    public function getCpm(int $type, int $param): mixed
+    {
+        return (new WildberriesData($this->getResponse(
+            'adv/v0/cpm',
+            compact('type', 'param')
         )))->data;
     }
 }
