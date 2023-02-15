82070c8612a5eac02b4373627d2223b7538b6713
filippovk997 <filippovk997@mail.ru>
2023-02-15 17:36:19 +0400

    deleted support old keys of statistic

--- src/WildberriesClient.php
+++ src/WildberriesClient.php
@@ -9,8 +9,6 @@ class WildberriesClient
 {
     private const STATISTICS_URL = 'https://statistics-api.wildberries.ru/';
     private const NON_STATISTICS_URL = 'https://suppliers-api.wildberries.ru/';
-    // @TODO: удалить после перехода на новые ключи WB (стандартный и статистика)
-    private const OLD_STATISTICS_URL = 'https://suppliers-stats.wildberries.ru/';
 
     private const DEFAULT_HEADER = [
         'Accept' => 'application/json',
@@ -39,8 +37,6 @@ class WildberriesClient
         $validator = Validator::make($keys, [
             'token_api' => 'required|string',
             'token_api_stat' => 'required|string',
-            // @TODO: удалить после перехода на новые ключи WB (стандартный и статистика)
-            'token_stat_x64' => 'string',
         ]);
 
         if ($validator->fails()) {
@@ -58,28 +54,10 @@ class WildberriesClient
      */
     protected function getResponse(string $uri = null, array $params = [], bool $is_stat = false): WildberriesResponse
     {
-        if ($this->config['token_api_stat'] !== 'NULL') {
-            // @TODO: оставить после перехода на новые ключи WB (стандартный и статистика)
-            $full_path = ($is_stat ? self::STATISTICS_URL : self::NON_STATISTICS_URL) . $uri;
-            $options = self::DEFAULT_OPTIONS;
+        $full_path = ($is_stat ? self::STATISTICS_URL : self::NON_STATISTICS_URL) . $uri;
+        $options = self::DEFAULT_OPTIONS;
 
-            $options['headers']['Authorization'] = $is_stat ? $this->config['token_api_stat'] : $this->config['token_api'];
-        } else {
-            // @TODO: удалить после перехода на новые ключи WB (стандартный и статистика)
-            $full_path = ($is_stat ? self::OLD_STATISTICS_URL : self::NON_STATISTICS_URL) . $uri;
-            $options = self::DEFAULT_OPTIONS;
-
-            if ($is_stat) {
-                $params['key'] = $this->config['token_stat_x64'];
-            } else {
-                $options['headers']['Authorization'] = $this->config['token_api'];
-            }
-        }
-        if ($is_stat) {
-            
-        } else {
-            $options['headers']['Authorization'] = $this->config['token_api'];
-        }
+        $options['headers']['Authorization'] = $is_stat ? $this->config['token_api_stat'] : $this->config['token_api'];
 
         if (count($params)) {
             $full_path .= '?' . http_build_query($params);
