00e3169592d820a071b7f2fb7992222eac1f2ce0
verzila99 <verzila0000@gmail.com>
2023-10-03 18:07:37 +0300

    adding advert media facade

---
+++ src/WildberriesAdvertMediaClient.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Filippi4\Wildberries;
+
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class WildberriesAdvertMediaClient
+{
+    private const ADVERT_URL = 'https://advert-media-api.wb.ru/';
+
+    private const DEFAULT_HEADER = [
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json',
+    ];
+
+    private const DEFAULT_OPTIONS = [
+        'headers' => self::DEFAULT_HEADER,
+        'timeout' => 35,
+        'connect_timeout' => 35,
+    ];
+
+    protected $config;
+
+    /**
+     * ClientHint constructor.
+     */
+    public function __construct()
+    {
+        $this->config = null;
+    }
+
+    /**
+     * @throws ValidationException
+     */
+    protected function validateKeys(array $keys): void
+    {
+        $validator = Validator::make($keys, [
+            'token_api_adv' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            throw ValidationException::withMessages($validator->messages()->getMessages());
+        }
+    }
+
+    /**
+     * Create GET request to bank API
+     *
+     * @param string|null $uri
+     * @param array $params
+     * @param bool $is_stat
+     * @return WildberriesResponse
+     */
+    protected function getResponse(string $uri = null, array $params = []): WildberriesResponse
+    {
+        $full_path = self::ADVERT_URL . $uri;
+        $options = self::DEFAULT_OPTIONS;
+
+        $options['headers']['Authorization'] = $this->config['token_api_adv'];
+
+        if (count($params)) {
+            $full_path .= '?' . http_build_query($params);
+        }
+
+        return WildberriesRequest::makeRequest($full_path, $options, 'get');
+    }
+}
