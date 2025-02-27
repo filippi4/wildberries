bfc897c7dfce442f7d4c1e77a02aff59f95c173a
DenEvtifeev <D.o.evtifeev@gmail.com>
2025-02-27 15:37:43 +0200

    adding WildberriesDocuments infrastructure

--- src/Wildberries.php
+++ src/Wildberries.php
@@ -749,16 +749,4 @@ class Wildberries extends WildberriesClient
         return (new WildberriesData($this->getResponse('api/v3/orders/new')))->data;
     }
 
-    public function getDocumentsList(array $params = []): mixed
-    {
-        return (new WildberriesData(
-            $this->getDocumentsResponse('api/v1/documents/list', $params)
-        ))->data;
-    }
-    public function downloadDocument(array $params = []): mixed
-    {
-        return (new WildberriesData(
-            $this->getDocumentsResponse('api/v1/documents/download', $params)
-        ))->data;
-    }
 }
